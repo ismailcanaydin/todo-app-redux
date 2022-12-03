899c58009aa686ffe24d4d6447e0f092ebbf0d15
ismailcanaydin <ismailcanaydin@gmail.com>
2022-12-03 16:36:37 +0300

    Yeni To-Do Elemanı Eklemek

--- src/redux/todos/todosSlice.js
+++ src/redux/todos/todosSlice.js
@@ -15,7 +15,14 @@ export const todosSlice = createSlice({
         }
         ],
     },
-    reducers: {},
+    //createSlice data klanlamayı kendi yapıyor.
+    //Buranın dışında işlem yapılacağı zaman datayı klanlamamız gerekli.
+    reducers: {
+        addTodo: (state, action) => {
+            state.items.push(action.payload)
+        }
+    },
 })
 
+export const { addTodo } = todosSlice.actions
 export default todosSlice.reducer
